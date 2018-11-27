5f16cc3f62d7dd131adf7ee4893873436e538bda
adas <asdas>
2018-11-27 15:34:15 -0500

    LOL

--- src/JackOLAntern.java
+++ src/JackOLAntern.java
@@ -4,6 +4,31 @@ public class JackOLAntern {
     public JackOLAntern(String[][] faceFeatures){
         this.faceFeatures=faceFeatures;
     }
-    
 
+
+    public void edit(String replace,int row, int column){
+        faceFeatures[row][column]=replace;
+
+    }
+
+    public void fill(String str){
+        for(int i=0; i<faceFeatures.length;i++){
+            for(int j=0;j<faceFeatures[i].length;j++){
+                faceFeatures[i][j]=str;
+            }
+        }
+
+    }
+
+    public String toString() {
+        String line="";
+        for(int i=0; i<faceFeatures.length;i++){
+            String row="";
+            for(int j=0;j<faceFeatures.length;j++){
+                row+=faceFeatures[i][j];
+            }
+            line+=row;
+        }
+        return line;
+    }
 }
--- src/JackOLanternRunner.java
+++ src/JackOLanternRunner.java
@@ -1,5 +1,21 @@
 public class JackOLanternRunner {
-    String[][] face=new String[5][8];
-    JackOLAntern face1= new JackOLAntern(face);
+    public static void main(String[]args) {
+        String[][] face = new String[5][8];
+        JackOLAntern face1 = new JackOLAntern(face);
 
+        face1.fill("x");
+        face1.edit("",0,0);
+        face1.edit("",4,7);
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+        face1.edit();
+    }
 }
